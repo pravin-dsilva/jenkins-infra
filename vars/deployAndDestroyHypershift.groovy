a1bebab6b39e4e786849884354c0824762720982
Pravin D'silva <pravin.dsilva@ibm.com>
2022-11-29 18:42:29 +0530

    hyp

--- vars/deployAndDestroyHypershift.groovy
+++ vars/deployAndDestroyHypershift.groovy
@@ -7,6 +7,10 @@ def call(){
             // Run the script for deploying hypershift
             sh '''
                 TAG="ppc64le"
+                rm -rf /usr/local/go
+                wget https://go.dev/dl/go1.18.8.darwin-amd64.tar.gz
+                tar -C /usr/local -xzf go1.18.8.darwin-amd64.tar.gz
+                export PATH=/usr/local/go/bin:$PATH
                 go version
                 cd ocp4-playbooks-extras
                 cp examples/hypershift_install.yaml vars.yaml
